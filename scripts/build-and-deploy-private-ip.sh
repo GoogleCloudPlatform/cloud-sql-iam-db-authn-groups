adc23cc838001eae5f174a9b9113a1e5174aec27
jackwotherspoon <jackwoth@google.com>
2022-05-12 17:55:01 +0000

    chore: update based on shellcheck

--- scripts/build-and-deploy-private-ip.sh
+++ scripts/build-and-deploy-private-ip.sh
@@ -33,8 +33,8 @@ export PATH_TO_JSON="" # file path to JSON payload containing instance-to-group 
 export SCHEDULE="*/10 * * * *" # schedule how often GroupSync Cloud Scheduler is called (defaults to 10 mins)
 
 # load IAM Groups and Cloud SQL Instance names from JSON payload
-IAM_GROUPS=`cat $PATH_TO_JSON | jq '.iam_groups' | tr -d '[],"'`
-SQL_INSTANCES=`cat $PATH_TO_JSON | jq '.sql_instances' | tr -d '[],"'`
+IAM_GROUPS=$(cat "$PATH_TO_JSON" | jq '.iam_groups' | tr -d '[],"')
+SQL_INSTANCES=$(cat "$PATH_TO_JSON" | jq '.sql_instances' | tr -d '[],"')
 
 # check if required variables are set, otherwise give error and exit
 declare -a vars=(PROJECT_ID REGION PATH_TO_JSON IAM_GROUPS SQL_INSTANCES CONNECTOR_NAME HOST_PROJECT_ID SUBNET)
@@ -48,10 +48,10 @@ done
 
 ######################## GCP PROJECT CONFIGURATION ########################
 # set project
-gcloud config set project $PROJECT_ID
+gcloud config set project "$PROJECT_ID"
 
 # set region
-gcloud config set compute/region $REGION
+gcloud config set compute/region "$REGION"
 
 # enable required APIs within project
 gcloud services enable run.googleapis.com cloudscheduler.googleapis.com \
@@ -60,23 +60,23 @@ gcloud services enable run.googleapis.com cloudscheduler.googleapis.com \
 
 ######################## SERVICE ACCOUNT CONFIGURATION ########################
 # create service account for use with GroupSync (REMOVE STEP IF USING PRE-EXISTING SERVICE ACCOUNT)
-gcloud iam service-accounts create $SERVICE_ACCOUNT_NAME \
+gcloud iam service-accounts create "$SERVICE_ACCOUNT_NAME" \
   --description="IAM Groups Authn Service Account" \
   --display-name="IAM Database Groups Authentication"
 
 # add Cloud Run Invoke Role to service account
-gcloud projects add-iam-policy-binding $PROJECT_ID \
+gcloud projects add-iam-policy-binding "$PROJECT_ID" \
   --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" \
   --role="roles/run.invoker"
 
 # create custom IAM role and grant to service account
 gcloud iam roles create IamAuthnGroups \
-  --project=$PROJECT_ID \
+  --project="$PROJECT_ID" \
   --title="IAM Groups Authn" \
   --description="Custom role for IAM DB Authn for Groups Service" \
   --permissions=cloudsql.instances.connect,cloudsql.instances.get,cloudsql.instances.login,cloudsql.users.create,cloudsql.users.list,iam.serviceAccounts.signBlob
 
-gcloud projects add-iam-policy-binding $PROJECT_ID \
+gcloud projects add-iam-policy-binding "$PROJECT_ID" \
   --member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" \
   --role="projects/$PROJECT_ID/roles/IamAuthnGroups"
 
@@ -84,45 +84,44 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
 # grant Cloud SQL Instance User role to all IAM group emails (so users of Group can inherit)
 for GROUP in $IAM_GROUPS;
 do
-  gcloud projects add-iam-policy-binding $PROJECT_ID \
+  gcloud projects add-iam-policy-binding "$PROJECT_ID" \
     --member="group:$GROUP" \
     --role="roles/cloudsql.instanceUser"
 done
-./
+
 # add service account as Cloud SQL IAM User to all mapped instances
 for INSTANCE in $SQL_INSTANCES;
 do
   IFS=: read -r PROJECT REGION_NAME INSTANCE_NAME <<< $INSTANCE
   gcloud sql users create $SERVICE_ACCOUNT_EMAIL \
-    --instance=$INSTANCE_NAME \
+    --instance="$INSTANCE_NAME" \
     --type=cloud_iam_service_account
 done
 
 ######################## PRIVATE IP / SHARED VPC ########################
 # create serverless VPC access connector
-gcloud compute networks vpc-access connectors create $CONNECTOR_NAME \
-  --region $REGION \
-  --subnet $SUBNET \
-  # If you are not using Shared VPC, omit the following line.
-  --subnet-project $HOST_PROJECT_ID 
-
+gcloud compute networks vpc-access connectors create "$CONNECTOR_NAME" \
+  --region="$REGION" \
+  --subnet="$SUBNET" \
+  --subnet-project="$HOST_PROJECT_ID" # if you are not using Shared VPC, omit this line.
+  
 ############################## CLOUD RUN ################################
 # build container for Cloud Run
 gcloud builds submit \
-  --tag gcr.io/$PROJECT_ID/groupsync-run \
-  --project $PROJECT_ID \
-  --region $REGION
+  --tag "gcr.io/$PROJECT_ID/groupsync-run" \
+  --project "$PROJECT_ID" \
+  --region "$REGION"
 
 # deploy Cloud Run service
 gcloud run deploy groupsync-run \
-  --image gcr.io/$PROJECT_ID/groupsync-run \
+  --image "gcr.io/$PROJECT_ID/groupsync-run" \
   --no-allow-unauthenticated \
-  --service-account $SERVICE_ACCOUNT_EMAIL \
-  --project $PROJECT_ID \
-  --region $REGION \
-  --vpc-connector $CONNECTOR_NAME
+  --service-account "$SERVICE_ACCOUNT_EMAIL" \
+  --project "$PROJECT_ID" \
+  --region "$REGION" \
+  --vpc-connector "$CONNECTOR_NAME"
 
-SERVICE_URL=`gcloud run services describe groupsync-run --platform managed --region $REGION --format 'value(status.url)'`
+SERVICE_URL=$(gcloud run services describe groupsync-run --platform managed --region "$REGION" --format 'value(status.url)')
 
 ########################### CLOUD SCHEDULER ############################
 # cloud scheduler command (schedules GroupSync to run every 10 minutes)
@@ -132,4 +131,4 @@ gcloud scheduler jobs create http groupsync-scheduler \
   --oidc-service-account-email=$SERVICE_ACCOUNT_EMAIL \
   --http-method="PUT" \
   --headers="Content-Type=application/json" \
-  --message-body-from-file=$PATH_TO_JSON
+  --message-body-from-file="$PATH_TO_JSON"
